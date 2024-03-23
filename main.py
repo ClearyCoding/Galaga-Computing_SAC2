1cf7188e2e02511e47a5726884361c32b698c14c
Alexander Cleary <alexjacleary@gmail.com>
2024-03-23 19:43:32 +1100

    Fixed For Loops, Moved Enemy De-spawning To Enemy Class, Support Firing Missiles For >3 Fighters

--- main.py
+++ main.py
@@ -51,42 +51,25 @@ class Game:
             screen.fill((0, 0, 0))
 
             # Draw Stars
-            for star in range(0, len(self.stars)):
-                if self.stars[star].y_coord > screen_height - 3:
-                    self.stars[star].regenerate()
+            for star in self.stars:
+                if star.y_coord > screen_height - 3:
+                    star.regenerate()
                 else:
-                    self.stars[star].tick()
+                    star.tick()
 
             # Draw Player
             self.player.tick()
 
             # Draw Missiles
-            for missile in range(len(self.player.missiles) - 1, -1, -1):
-                if self.player.missiles[missile].y_coord < 3:
-                    del self.player.missiles[missile]
+            for missile in self.player.missiles:
+                if missile.y_coord < 3:
+                    self.player.missiles.remove(missile)
                 else:
-                    self.player.missiles[missile].tick()
+                    missile.tick()
 
             # Draw Enemies
-            for enemy in range(len(self.enemies) - 1, -1, -1):
-                for missile in range(len(self.player.missiles) - 1, -1, -1):
-                    try:
-                        if abs(self.enemies[enemy].x_coord - self.player.missiles[missile].x_coord) < self.enemies[
-                                enemy].width / 2 + self.player.missiles[missile].width / 2 and abs(
-                                self.enemies[enemy].y_coord - self.player.missiles[missile].y_coord) < self.enemies[
-                                enemy].height / 2 + self.player.missiles[missile].height / 2:
-                            del self.player.missiles[missile]
-                            self.enemies[enemy].health -= 1
-                            if self.enemies[enemy].health <= 0:
-                                self.enemies[enemy].death_sound.play()
-                                del self.enemies[enemy]
-                            else:
-                                self.enemies[enemy].hurt_sound.play()
-                    except IndexError:
-                        continue
-
-            for enemy in range(len(self.enemies)):
-                self.enemies[enemy].tick()
+            for enemy in self.enemies:
+                enemy.tick()
 
             # Refresh Screen
             pygame.display.flip()
@@ -103,7 +86,7 @@ class Game:
                     y_coord = 60 + (45 * row)
                     x_coord = 97.5 + (45 * column)
 
-                    self.enemies.append(Enemy(species, x_coord, y_coord))
+                    self.enemies.append(Enemy(species, x_coord, y_coord, self))
 
 
 class Star:
@@ -138,9 +121,10 @@ class Star:
 class Player:
     def __init__(self):
         self.lives = 3
-        self.fighters = 1
+        self.fighters = 10
         self.height = 15 * sizeMultiplier
-        self.width = 15 * sizeMultiplier * self.fighters
+        self.fighter_width = 15 * sizeMultiplier
+        self.width = self.fighter_width * self.fighters
 
         self.x_coord = screen_width / 2 - self.width / 2
         self.y_coord = screen_height - 80
@@ -150,7 +134,7 @@ class Player:
         self.missiles = []
         self.firing_sound = pygame.mixer.Sound('assets/sounds/firing.mp3')
 
-        self.image = pygame.image.load(['assets/player0a.png', 'assets/player0b.png'][self.fighters - 1])
+        self.image = pygame.image.load(['assets/player0a.png', 'assets/player0b.png', 'assets/player0c.png'][self.fighters - 1])
         self.image = pygame.transform.scale(self.image, (self.width, self.height))
 
     def tick(self):
@@ -171,19 +155,15 @@ class Player:
 
     def shoot(self):
         if len(self.missiles) < 2 * self.fighters:
-            if self.fighters == 1:
-                self.firing_sound.play()
-                self.missiles.append(Missile(self.x_coord + self.width / 2))
-            elif self.fighters == 2:
-                self.firing_sound.play()
-                self.missiles.append(Missile((self.x_coord + self.width / 4) + self.width / 2))
-                self.missiles.append(Missile((self.x_coord - self.width / 4) + self.width / 2))
+            self.firing_sound.play()
+            for fighter in range(self.fighters):
+                self.missiles.append(Missile(self.x_coord + self.fighter_width / 2 + fighter * self.fighter_width))
 
 
 class Missile:
     def __init__(self, x_coord):
         self.x_coord = x_coord
-        self.y_coord = screen_height - 100
+        self.y_coord = screen_height - 80
 
         self.width = 3 * sizeMultiplier
         self.height = 8 * sizeMultiplier
@@ -193,16 +173,17 @@ class Missile:
 
     def tick(self):
         if self.y_coord > 3:
-            self.y_coord -= 20
+            self.y_coord -= 30
 
         screen.blit(self.image, (self.x_coord - self.width / 2, self.y_coord))
 
 
 class Enemy:
-    def __init__(self, species=0, x_coord=screen_width/2, y_coord=60):
+    def __init__(self, species=0, x_coord=screen_width/2, y_coord=60, game=None):
         self.species = species
         self.x_coord = x_coord
         self.y_coord = y_coord
+        self.game = game
         self.tick_delay = 0
         self.health = [1, 1, 2][self.species]
         self.death_sound = pygame.mixer.Sound(['assets/sounds/enemy_death_a.mp3', 'assets/sounds/enemy_death_b.mp3',
@@ -219,6 +200,7 @@ class Enemy:
 
     def tick(self):
         self.tick_delay += 1
+        self.check_collision()
         if self.tick_delay % 15 == 0:
             self.animated = not self.animated
 
@@ -233,6 +215,21 @@ class Enemy:
 
         self.image = pygame.transform.scale(self.image, (self.width, self.height))
         screen.blit(self.image, (self.x_coord - self.width / 2, self.y_coord))
+
+    def check_collision(self):
+        for missile in self.game.player.missiles:
+            if (abs(self.x_coord - missile.x_coord) <
+                    self.width / 2 + missile.width / 2 and abs(
+                    self.y_coord - missile.y_coord) <
+                    self.height / 2 + missile.height / 2):
+                self.game.player.missiles.remove(missile)
+                self.health -= 1
+                if self.health <= 0:
+                    self.death_sound.play()
+                    if self in self.game.enemies:
+                        self.game.enemies.remove(self)
+                else:
+                    self.hurt_sound.play()
 
 
 myGame = Game()
