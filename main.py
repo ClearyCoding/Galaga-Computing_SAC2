31b02f036af9fa4bc099919892b5e6bbd6d46c8f
Alexander Cleary <alexjacleary@gmail.com>
2024-03-23 21:18:04 +1100

    Added Explosions

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import pygame
 import random
+import math
 
 highScores = {}
 sizeMultiplier = 2.5
@@ -25,6 +26,7 @@ class Game:
         self.player = Player()
         self.stars = []
         self.enemies = []
+        self.explosions = []
         self.clock = pygame.time.Clock()
         self.tps = 30
 
@@ -70,6 +72,9 @@ class Game:
             # Draw Enemies
             for enemy in self.enemies:
                 enemy.tick()
+
+            for explosion in self.explosions:
+                explosion.tick()
 
             # Refresh Screen
             pygame.display.flip()
@@ -144,7 +149,7 @@ class Player:
         if keys[pygame.K_RIGHT]:
             self.move(1)
         for fighter in range(self.fighters):
-            screen.blit(self.image, (self.x_coord + self.fighter_width * fighter, self.y_coord))
+            screen.blit(self.image, (self.x_coord + self.fighter_width * fighter, self.y_coord - self.height / 2))
 
     def move(self, direction):
         if direction == -1:
@@ -180,11 +185,11 @@ class Missile:
         if self.y_coord > 3:
             self.y_coord -= 30
 
-        screen.blit(self.image, (self.x_coord - self.width / 2, self.y_coord))
+        screen.blit(self.image, (self.x_coord - self.width / 2, self.y_coord - self.height / 2))
 
 
 class Enemy:
-    def __init__(self, species=0, x_coord=screen_width/2, y_coord=60, game=None):
+    def __init__(self, species=0, x_coord=screen_width / 2, y_coord=60, game=None):
         self.species = species
         self.x_coord = x_coord
         self.y_coord = y_coord
@@ -219,22 +224,51 @@ class Enemy:
                                            [self.species])
 
         self.image = pygame.transform.scale(self.image, (self.width, self.height))
-        screen.blit(self.image, (self.x_coord - self.width / 2, self.y_coord))
+        screen.blit(self.image, (self.x_coord - self.width / 2, self.y_coord - self.height / 2))
 
     def check_collision(self):
         for missile in self.game.player.missiles:
             if (abs(self.x_coord - missile.x_coord) <
                     self.width / 2 + missile.width / 2 and abs(
-                    self.y_coord - missile.y_coord) <
+                        self.y_coord - missile.y_coord) <
                     self.height / 2 + missile.height / 2):
                 self.game.player.missiles.remove(missile)
                 self.health -= 1
                 if self.health <= 0:
-                    self.death_sound.play()
-                    if self in self.game.enemies:
-                        self.game.enemies.remove(self)
+                    self.die()
                 else:
                     self.hurt_sound.play()
+
+    def die(self):
+        self.death_sound.play()
+        self.game.explosions.append(Explosion(self.x_coord + self.width / 2, self.y_coord + self.height / 2, self.game))
+        if self in self.game.enemies:
+            self.game.enemies.remove(self)
+
+
+class Explosion:
+    def __init__(self, x_coord, y_coord, game):
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.game = game
+        self.tick_delay = 0
+        self.animation = 0
+        self.size = 32
+        self.image = pygame.image.load(['assets/explosion0.png', 'assets/explosion1.png', 'assets/explosion2.png',
+                                        'assets/explosion3.png', 'assets/explosion4.png']
+                                       [math.floor(self.tick_delay / 3)])
+
+    def tick(self):
+        self.tick_delay += 1
+        self.animation = math.floor(self.tick_delay / 3)
+        if self.animation > 4:
+            self.game.explosions.remove(self)
+        else:
+            self.image = pygame.image.load(['assets/explosion0.png', 'assets/explosion1.png', 'assets/explosion2.png',
+                                            'assets/explosion3.png', 'assets/explosion4.png']
+                                           [self.animation])
+        self.image = pygame.transform.scale(self.image, (self.size, self.size))
+        screen.blit(self.image, (self.x_coord - self.size, self.y_coord - self.size))
 
 
 myGame = Game()
